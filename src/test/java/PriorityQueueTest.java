e12fbdbb83297ae171406d75de6d9bb58d0e366e
ChenPinChing <p.ching0811@gmail.com>
2022-03-16 18:50:56 +0800

    Test fail test

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -14,7 +14,7 @@ class testPriorityQueueTest {
     private static Stream<Arguments> provideTestData()
     {
         return Stream.of(
-                Arguments.of(new int[]{1 ,2, 4, 3}, new int[]{1, 2, 3, 4}),
+                Arguments.of(new int[]{0, 1 ,2, 4, 3}, new int[]{1, 2, 3, 4}),
                 Arguments.of(new int[]{3, 8, 18, 32}, new int[]{3, 8, 18, 32}),
                 Arguments.of(new int[]{31, 7, 8, 22}, new int[]{7, 8, 22, 31}),
                 Arguments.of(new int[]{64, 54, 52, 37}, new int[]{37, 52, 54, 64}),
@@ -40,18 +40,28 @@ class testPriorityQueueTest {
     }
 
     @Test
-    public void whenExceptionThrown(){
+    public void whenExceptionThrown_ClassCastException(){
         PriorityQueue<Object> ObjectQueue = new PriorityQueue<>();
         ObjectQueue.add(1);
 
         assertThrows(ClassCastException.class,
                 ()->{System.out.println((String) ObjectQueue.poll());});
 
+
+    }
+
+    @Test
+    public void whenExceptionThrown_NullPointerException()
+    {
         PriorityQueue<Object> NullQueue = new PriorityQueue<>();
 
         assertThrows(NullPointerException.class,
                 ()->{NullQueue.add(null);});
+    }
 
+    @Test
+    public void whenExceptionThrown_IllegalArgumentException()
+    {
         assertThrows(IllegalArgumentException.class,
                 ()->{ PriorityQueue<Object> QQueue = new PriorityQueue<>(-1);;});
     }
